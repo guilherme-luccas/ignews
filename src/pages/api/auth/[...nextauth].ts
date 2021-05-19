5fb25709dbd9cd5b46d442555b0ab436e4823147
Guilherme Luccas <guilherme.luccas@outlook.com>
2021-05-19 18:03:10 -0300

    erro fauna

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID, // VARIAVEL AMBIENTE NO ARQUIVO ENV.LOCAL
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      scope: "read:user",
+    }),
+    // ...add more providers here
+  ],
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("users_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index("users_by_email"), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
